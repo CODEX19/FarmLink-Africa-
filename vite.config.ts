774200662ad8e2b127ec5eedc1a80565ff6068a2
Tobijah Uzebu <141057250+CODEX19@users.noreply.github.com>
2026-01-07 18:29:58 +0000

    fix vite.config.ts for React + Netlify
    Updated Vite configuration to load environment variables based on base instead of mode.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+//https://vitejs.dev/config/
+export default defineConfig(({ base }) => {
+    const env = loadEnv(base, './', //ensures assets load correctly when deployed);
     return {
       server: {
         port: 3000,
